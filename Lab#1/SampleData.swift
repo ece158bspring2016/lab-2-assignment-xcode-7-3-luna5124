8f99b5a0f8ff40f1fd40647d1cadd7d0b0328c34
Lihui Lu <lunarlu5124@gmail.com>
2016-04-22 00:00:46 -0700

    lab session 1

---
+++ Lab#1/SampleData.swift
@@ -0,0 +1,14 @@
+//
+//  SampleData.swift
+//  Lab#1
+//
+//  Created by Lihui(Luna) Lu on 4/21/16.
+//  Copyright © 2016 luna app. All rights reserved.
+//
+
+import Foundation
+
+let playersData = [
+    Player(name:"Bill Evans", game:"Tic-Tac-Toe", rating: 4),
+    Player(name: "Oscar Peterson", game: "Spin the Bottle", rating: 5),
+    Player(name: "Dave Brubeck", game: "Texas Hold 'em Poker", rating: 2) ]
